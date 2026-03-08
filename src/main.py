ad70de833be64bb60f4af82b766a0ae766c1d4c5
Alexoko123 <koko@cub.uca.edu>
2026-03-08 15:15:56 -0500

    Added DecisionTree script

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+from readline import backend
+
+from model import DecisionTree
+from model import model_backend
+from model import model_driver
 from model.model_driver import Knn_model as model
 import os
 
@@ -17,6 +22,8 @@ if __name__ == "__main__":
     print('DataMining: Project')
     print()
     
+    #== instance of model_driver
+    backend = model_backend.Backend()
     # ===== config variables
     datasetFile = "dataset/Social_Media_Mental_Health.csv"
     
@@ -38,8 +45,34 @@ if __name__ == "__main__":
     # === k fold validation 
     knn.run_k_fold_validation(Kfolds)
 
+    # ===Decision Tree
+    training_data, testing_data = backend.train_test_split(datasetFile)
+
+    features_train, features_test = backend.features_train_test(training_data, testing_data)
+
+    labels_train, labels_test = backend.labels_train_test(training_data, testing_data)
+
+    X_train = features_train.to_numpy()
+    y_train = labels_train.to_numpy()
+
+    tree = DecisionTree(max_depth=5)
+    tree.fit(X_train, y_train)
+
+    X_test = features_test.to_numpy()
+    y_test = labels_test.to_numpy()
+
+    predictions = tree.predict(X_test)
+
+    for i in range(len(predictions)):
+        print("Prediction:", predictions[i], "| Actual:", y_test[i])
+
     # === final model evaluation
     #knn.model_evaluation()
+
+    backend.test_labels = labels_test
+    backend.predictions = predictions
+
+    backend.model_evaluation()
     
 
 # DataPoint = each piece of data from the dataset
