fdc23db9ae302c0f6b11aedbfe6ec48db2c85ae9
jgmcclain91uca <jgmcclain91@gmail.com>
2026-03-18 10:50:42 -0500

    Update model_backend.py

--- src/model/model_backend.py
+++ src/model/model_backend.py
@@ -16,13 +16,13 @@ class Backend:
     def load_dataset(self, datasetFile, viewRawDataset):
 
         print("Reading in File: ", datasetFile)
-        
+
         # --- tries to read file
         try:
-            Mental_Health_Data = pd.read_csv(datasetFile) 
+            Mental_Health_Data = pd.read_csv(datasetFile)
 
             # peek raw data, dependent on flag
-            if(viewRawDataset):
+            if (viewRawDataset):
                 print("raw dataset | peeking data frame head:")
                 print(Mental_Health_Data.head())
 
@@ -34,7 +34,7 @@ class Backend:
         return Mental_Health_Data
 
     # Normalization   
-    def normalize_data(self, Mental_Health_Data): 
+    def normalize_data(self, Mental_Health_Data):
 
         ''' 
         X = features
@@ -45,7 +45,7 @@ class Backend:
         '''
         Normalize_cols = Mental_Health_Data.select_dtypes(include=['int64', 'float64']).columns
 
-        #loop through my Columns
+        # loop through my Columns
         for col in Normalize_cols:
             # Step 1 Find min and max
             min_val = Mental_Health_Data[col].min()
@@ -55,9 +55,10 @@ class Backend:
             if max_val != min_val:
                 Mental_Health_Data[col] = (Mental_Health_Data[col] - min_val) / (max_val - min_val)
 
-        return Mental_Health_Data    
+        return Mental_Health_Data
 
-    # Convert to Binary 
+        # Convert to Binary
+
     def binary_encoding(self, Mental_Health_Data):
 
         #        -- Activity Type
@@ -66,21 +67,21 @@ class Backend:
             "Passive": 0
         })
 
-        #-- Gender
+        # -- Gender
         Mental_Health_Data["Gender"] = Mental_Health_Data["Gender"].map({
             "Male": 1,
             "Female": 0
         })
-        
-        #--User_Archetype
+
+        # --User_Archetype
         Mental_Health_Data["User_Archetype"] = Mental_Health_Data["User_Archetype"].map({
             "Digital Minimalist": 0,
             "Passive Scroller": 0,
             "Hyper-Connected": 1,
             "Average User": 1
         })
-        
-        #---- Age Normalize it?
+
+        # ---- Age Normalize it?
         """Mental_Health_Data["User_Archetype"] = Mental_Health_Data["User_Archetype"].map({
             "18": 0,
             "19": 0,
@@ -88,8 +89,8 @@ class Backend:
             "21": 1,
             "22": 1
         })"""
-        
-      #-- Primary_Platform
+
+        # -- Primary_Platform
         Mental_Health_Data["Primary_Platform"] = Mental_Health_Data["Primary_Platform"].map({
             "Facebook": 0,
             "Twitter/X": 0,
@@ -99,7 +100,7 @@ class Backend:
             "TikTok": 1,
             "Instagram": 1
         })
-        
+
         # Content type
         Mental_Health_Data["Dominant_Content_Type"] = Mental_Health_Data["Dominant_Content_Type"].map({
             "Gaming": 0,
@@ -110,9 +111,9 @@ class Backend:
             "Educational/Tech": 1
         })
 
-        #-- GAD_7_Severity
-        #0 = No/Low anxiety → Minimal, Mild
-        #1 = Clinically significant anxiety → Moderate, Severe
+        # -- GAD_7_Severity
+        # 0 = No/Low anxiety → Minimal, Mild
+        # 1 = Clinically significant anxiety → Moderate, Severe
         Mental_Health_Data["GAD_7_Severity"] = Mental_Health_Data["GAD_7_Severity"].map({
             "Mild": 0,
             "Minimal": 0,
@@ -120,14 +121,14 @@ class Backend:
             "Severe": 1
         })
 
-         #-- PHQ_9 Severity
-         #Target Label
+        # -- PHQ_9 Severity
+        # Target Label
         Mental_Health_Data["PHQ_9_Severity"] = Mental_Health_Data["PHQ_9_Severity"].map({
-          "Mild":0,
-          'None-Minimal':0,
-          'Moderate':1,
-          'Moderately Severe':1,
-          'Severe':1
+            "Mild": 1,
+            'None-Minimal': 0,
+            'Moderate': 1,
+            'Moderately Severe': 1,
+            'Severe': 1
         })
 
         Mental_Health_Data = Mental_Health_Data.drop(columns=["User_ID"])
@@ -136,25 +137,25 @@ class Backend:
     # ================ phase 2 : data splitting & prep for training
     # 80/20 Split
     def train_test_split(self, Mental_Health_Data):
-        
-        #---- 80/20 Split
+
+        # ---- 80/20 Split
         # find my index for 80% of dataset
         split_index = int(0.8 * len(Mental_Health_Data))
-        
-        #split based on range of 80%
+
+        # split based on range of 80%
         training_data = Mental_Health_Data.iloc[:split_index]
         testing_data = Mental_Health_Data.iloc[split_index:]
 
         return training_data, testing_data
-    
+
     # Generate Train/Test Inputs  
-    def features_train_test(self, training_data, testing_data):  
-        
+    def features_train_test(self, training_data, testing_data):
+
         # remove severity & score from training | score heavily predicts severity 
-        drop_cols = ["PHQ_9_Severity", "PHQ_9_Score", "GAD_7_Severity", "GAD_7_Score"]
-            
+        drop_cols = ["Grade class"]
+
         features_train = training_data.drop(drop_cols, axis=1)
-        features_test = testing_data.drop(drop_cols, axis=1)     
+        features_test = testing_data.drop(drop_cols, axis=1)
 
         return features_train, features_test
 
@@ -162,12 +163,12 @@ class Backend:
     def truth_train_test(self, training_data, testing_data, label_choice):
 
         if label_choice == 'anxiety_severity':
-            ground_truth_train = training_data["GAD_7_Severity"]
-            ground_truth_test = testing_data["GAD_7_Severity"]
-        
+            ground_truth_train = training_data["Grade class"]
+            ground_truth_test = testing_data["Grade class"]
+
         else:
-            ground_truth_train = training_data["PHQ_9_Severity"]
-            ground_truth_test = testing_data["PHQ_9_Severity"]
+            ground_truth_train = training_data["Grade class"]
+            ground_truth_test = testing_data["Grade class"]
 
         return ground_truth_train, ground_truth_test
 
@@ -182,69 +183,69 @@ class Backend:
         '''
         left_tree_idxs = np.argwhere(feature_column <= threshold).flatten()
         right_tree_idxs = np.argwhere(feature_column > threshold).flatten()
-        
+
         return left_tree_idxs, right_tree_idxs
-    
+
     # finds most common label | ground_truths = labels
     def most_common_label(self, ground_truths):
-    
+
         counter = Counter(ground_truths.flatten())
         return counter.most_common(1)[0][0]
 
     # calculates entropy for information gain
     def entropy(self, condition_prob):
-        if(condition_prob == 0):
+        if (condition_prob == 0):
             return 0
-       
-        else: 
+
+        else:
             # entropy = -sum( P_i * log_2(P_i) )
             entropy_val = -(condition_prob * log2(condition_prob))
             return entropy_val
 
     # generates informatin gain 
     def information_gain(self, ground_truth, features, threshold):
-        
+
         # gather indices of left & right sub trees
         left_tree_idxs, right_tree_idxs = self.split(features, threshold)
 
         # stop if subtree is empty
         if len(left_tree_idxs) == 0 or len(right_tree_idxs) == 0:
             return 0
-       
+
         # length of :: truth column; left subtree; and right subtree
         truth_column_length = len(ground_truth)
         left_subtree_length, right_subtree_length = len(left_tree_idxs), len(right_tree_idxs)
 
         # gather frequencies 
-        truth_frequencies       = np.bincount(ground_truth.flatten().astype(int), minlength=2)        
-        left_truth_frequencies  = np.bincount(ground_truth[left_tree_idxs].flatten().astype(int), minlength=2)
+        truth_frequencies = np.bincount(ground_truth.flatten().astype(int), minlength=2)
+        left_truth_frequencies = np.bincount(ground_truth[left_tree_idxs].flatten().astype(int), minlength=2)
         right_truth_frequencies = np.bincount(ground_truth[right_tree_idxs].flatten().astype(int), minlength=2)
 
         # calculate entropy 
-        truth_entropy   = sum(self.entropy(freq / truth_column_length) for freq in truth_frequencies)
-        left_entropy    = sum(self.entropy(freq / left_subtree_length) for freq in left_truth_frequencies)
-        right_entropy   = sum(self.entropy(freq / right_subtree_length) for freq in right_truth_frequencies)
+        truth_entropy = sum(self.entropy(freq / truth_column_length) for freq in truth_frequencies)
+        left_entropy = sum(self.entropy(freq / left_subtree_length) for freq in left_truth_frequencies)
+        right_entropy = sum(self.entropy(freq / right_subtree_length) for freq in right_truth_frequencies)
 
         # weighted entropy = 
         # (condition_1 prob)(entropy_1) +...+ (condition_n prob)(entropy_n)
         prob_1 = left_subtree_length / truth_column_length
         prob_2 = right_subtree_length / truth_column_length
-        
+
         weighted_entropy = (prob_1 * left_entropy) + (prob_2 * right_entropy)
 
-        #information gain = traget entropy - weighted entropy
+        # information gain = traget entropy - weighted entropy
         information_gain = truth_entropy - weighted_entropy
         return information_gain
-    
+
     # uses the information gain to find the best partition  
     def best_split(self, features, ground_truth, total_features):
-        
-        best_gain = -1 
+
+        best_gain = -1
         split_idx, split_threshold = None, None
-        
+
         # find threshold & index that splits into the best subtree
         for feature_idx in range(total_features):
-            
+
             # isolated feature column
             feature_column = features[:, feature_idx]
 
@@ -271,8 +272,7 @@ class Backend:
     # ================ phase 4 : K fold validation 
     # 4.0: Define Folder Function
     def generate_k_folds(self, data, K):
-    
-        data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+
         # fold size = data size / number of folds
         # data = 100 | k = 5 | fold_size = 100 / 5 = 20
         fold_size = len(data) // K
@@ -284,20 +284,19 @@ class Backend:
             data partitioning into folds | start & stop indexing
             start = 1 * 20 = 20 | i = 1 ; fold = 20  
             end = 20 + 20 = 40  | fold = indx 20 -> indx 40 
-            ''' 
+            '''
             start = i * fold_size
             end = start + fold_size
-            
+
             # last fold remainder | i = k -1
-            if i == K - 1:  
+            if i == K - 1:
                 fold = data.iloc[start:]
             else:
                 fold = data.iloc[start:end]
-            
+
             # adds each partition (fold) to container
             fold_container.append(fold)
 
         return fold_container
 
-
-    # ================ phase 5 : model evaluations 
+    # ================ phase 5 : model evaluations
